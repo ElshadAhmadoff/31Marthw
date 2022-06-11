4990ee052eca37770023f58ca9495b28685cd708
ElshadAhmadof <744s72c@code.edu.az>
2022-06-11 18:19:21 +0400

    Homework

---
+++ 31Mart/31Mart/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_04_2022_Homework
+{
+    internal static class Calculator
+    {
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+     
+
+        public static double Divide(int n1, int n2)
+        {
+            return (double)n1 / (double)n2;
+        }
+
+
+        public static int Subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public static int Multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public static int Pow(this int variable, int a)
+        {
+            int pow = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                pow = pow * variable;
+            }
+            return pow;
+        }
+    }
+}
---
+++ 31Mart/31Mart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_04_2022_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Calculator.Sum(n1, n2));
+            Console.WriteLine(Calculator.Multiply(n1, n2));
+            Console.WriteLine(Calculator.Divide(n1, n2));
+            Console.WriteLine(Calculator.Subtract(n1, n2));
+            Console.WriteLine(n1.Pow(n2));
+            Console.ReadLine();
+        }
+    }
+}
